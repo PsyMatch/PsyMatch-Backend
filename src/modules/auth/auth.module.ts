2e690692f801f0db18de08824205be76eae3301f
faustopavan12 <faustopavan2003@gmail.com>
2025-08-05 17:33:50 -0300

    Jwt-Config

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,11 +3,24 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { QueryTransactionHelper } from '../utils/helpers/queryTransaction.helper';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, JwtService],
+  providers: [AuthService, QueryTransactionHelper],
 })
 export class AuthModule {}
